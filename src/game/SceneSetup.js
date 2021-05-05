99e05ebad0e4e3e8cac7c32441da761f6b66b939
Chad Dayton <cdayton96@gmail.com>
2021-05-04 22:31:19 -0400

    feat: SceneSetup component to init Babylon & Game to handle cams, lights, control

---
+++ src/game/SceneSetup.js
@@ -0,0 +1,60 @@
+import { Engine, Scene } from "@babylonjs/core";
+import React, { useEffect, useRef } from "react";
+
+const SceneSetup = (props) => {
+  const canvas = useRef(null);
+  const {
+    antialias,
+    engineOptions,
+    adaptToDeviceRatio,
+    sceneOptions,
+    onRender,
+    onSceneReady,
+    ...rest
+  } = props;
+
+  useEffect(() => {
+    if (canvas.current) {
+      const engine = new Engine(
+        canvas.current,
+        antialias,
+        engineOptions,
+        adaptToDeviceRatio
+      );
+      const scene = new Scene(engine, sceneOptions);
+
+      if (scene.isReady()) {
+        props.onSceneReady(scene);
+      } else {
+        scene.onReadyObservable.addOnce((scene) => props.onSceneReady(scene));
+      }
+
+      engine.runRenderLoop(() => {
+        if (typeof onRender === "function") {
+          onRender(scene);
+        }
+        scene.render();
+      });
+
+      const resize = () => {
+        scene.getEngine().resize();
+      };
+
+      if (window) {
+        window.addEventListener("resize", resize);
+      }
+
+      return () => {
+        scene.getEngine().dispose();
+
+        if (window) {
+          window.removeEventListener("resize", resize);
+        }
+      };
+    }
+  }, [canvas]);
+
+  return <canvas ref={canvas} {...rest} />;
+};
+
+export default SceneSetup;
--- src/game/game.js
+++ src/game/game.js
@@ -1,13 +1,60 @@
-//core game logic, run/render loops, char/cam settings, scenes, lights, canvas definition,
+import React from "react";
 import {
-  Engine,
-  Scene,
-  Vector3,
-  Mesh,
-  MeshBuilder,
   FreeCamera,
-  FollowCamera,
-  Color3,
-} from '@babylonjs/core';
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+} from "@babylonjs/core";
+import SceneSetup from "./SceneSetup";
 
-export default function game() {}
+let box;
+
+const onSceneReady = (scene) => {
+  // This creates and positions a free camera (non-mesh)
+  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+  // This targets the camera to scene origin
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0
+  var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+  light.intensity = 0.7;
+
+  // Box model setup
+  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+  box.position.y = 1;
+
+  // Ground setup
+  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+};
+
+// Will run on every frame render.  We are spinning the box on the y-axis.
+
+const onRender = (scene) => {
+  if (box !== undefined) {
+    var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+export default function Game() {
+  return (
+    <div>
+      <SceneSetup
+        antialias
+        onSceneReady={onSceneReady}
+        onRender={onRender}
+        id="gameCanvas"
+      />
+    </div>
+  );
+}
